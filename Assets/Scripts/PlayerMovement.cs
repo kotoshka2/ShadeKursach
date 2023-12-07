468c482e542dfa45e79fa6e7ff85477e3cf1ce81
Kotoshka2 <myrzabekov.madiyar@mail.ru>
2023-12-08 03:02:28 +0600

    1234

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -64,11 +64,6 @@ public class PlayerMovement : MonoBehaviour, IMoveable
         {
             
             rb.AddForce(Vector2.up * jumpPower);
-<<<<<<< HEAD
-
-=======
-            
->>>>>>> dfaeb1ad33507a5de2c90ae1d33cc4646045d3b9
         }
         
     }
